0e222045e9c2de8fb649c365b156f72cde34eb50
dulecux <dusan.culibrk@live.com>
2021-08-25 23:15:51 +0200

    Blog is finished

---
+++ src/views/Details.vue
@@ -0,0 +1,52 @@
+<template>
+  <div v-if="error">{{ error }}</div>
+  <div v-if="post" class="post">
+    <h3>{{ post.title }}</h3>
+    <p class="pre">{{ post.body }}</p>
+  </div>
+  <div v-else>
+      <Spinner />
+  </div>
+</template>
+
+<script>
+import getPost from '@/composables/getPost'
+import { useRoute } from 'vue-router'
+
+import Spinner from '../components/Spinner.vue'
+
+
+export default {
+  components: { Spinner },
+  props: ['id'],
+  setup(props) {
+
+    const route = useRoute()
+    // console.log(route)
+    // console.log(route.params)
+
+    //props.id ili route.params.id
+    const { error, post, load } = getPost(route.params.id)
+    load()
+    return { error, post }
+  },
+}
+</script>
+
+<style scoped>
+  .tags a {
+    margin-right: 10px;
+  }
+  .post {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  .post p {
+    color: #444;
+    line-height: 1.5em;
+    margin-top: 40px;
+  }
+  .pre {
+    white-space: pre-wrap;
+  }
+</style>
